2015d7668b915c21dc3a7cd5be9ce6fb85bbfa48
mgi166 <skskoari@gmail.com>
2015-05-11 07:55:35 +0900

    Add Valanga::Client#login * use `Mechanize` gem

--- lib/valanga/client.rb
+++ lib/valanga/client.rb
@@ -1,4 +1,23 @@
+require 'mechanize'
+
 module Valanga
   class Client
+    LOGIN_PAGE = "https://p.eagate.573.jp/gate/p/login.html"
+
+    def initialize
+      @mechanize = Mechanize.new do |mech|
+        mech.keep_alive = false
+      end
+    end
+
+    def login(username, password)
+      @mechanize.get(LOGIN_PAGE) do |page|
+        page.form_with do |f|
+          f.encoding = 'Shift_JIS'
+          f['KID']  = username
+          f['pass'] = password
+        end.submit
+      end
+    end
   end
 end
