99d8b158cab31f266a907eba6bb0e33dd1a50bc7
mgi166 <skskoari@gmail.com>
2015-05-16 14:05:42 +0900

    Remove mechanized code

--- lib/valanga/client.rb
+++ lib/valanga/client.rb
@@ -1,35 +1,15 @@
-require 'mechanize'
-
 module Valanga
   class Client
     LOGIN_PAGE = "https://p.eagate.573.jp/gate/p/login.html"
     MUSIC_INDEX = "http://p.eagate.573.jp/game/reflec/groovin/p/music/index.html"
 
     def initialize
-      @mechanize = Mechanize.new do |mech|
-        mech.keep_alive = false
-      end
     end
 
     def login(username, password)
-      @mechanize.get(LOGIN_PAGE) do |page|
-        page.form_with do |f|
-          f.encoding = 'Shift_JIS'
-          f['KID']  = username
-          f['pass'] = password
-        end.submit
-      end
-
-      if logged_in?
-        true
-      else
-        raise
-      end
     end
 
     def logged_in?
-      page = @mechanize.get(MUSIC_INDEX)
-      ! page.links.find { |link| link.text =~ /ログイン/ }
     end
   end
 end
