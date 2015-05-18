228af4a47ea2da54c0b4c6d82335fec84b7ab753
mgi166 <skskoari@gmail.com>
2015-05-18 23:28:53 +0900

    Updates Valanga::Client#login for using capybara/poltergeist

--- lib/valanga/client.rb
+++ lib/valanga/client.rb
@@ -1,12 +1,28 @@
+require 'capybara'
+require 'capybara/poltergeist'
+
 module Valanga
   class Client
-    LOGIN_PAGE = "https://p.eagate.573.jp/gate/p/login.html"
-    MUSIC_INDEX = "http://p.eagate.573.jp/game/reflec/groovin/p/music/index.html"
+    LOGIN_PAGE  = "https://p.eagate.573.jp/gate/p/login.html"
+    MUSIC_INDEX = "https://p.eagate.573.jp/game/reflec/groovin/p/music/index.html"
+
+    attr_reader :session
 
     def initialize
+      Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(app)
+      end
+
+      @session = Capybara::Session.new(:poltergeist)
     end
 
     def login(username, password)
+      @session.visit LOGIN_PAGE
+
+      @session.fill_in 'KID', with: username
+      @session.fill_in 'pass', with: password
+
+      @session.click_on "規約に同意してログイン"
     end
 
     def logged_in?
