8dca35e2874c05d5ff3cbc5c83d1af1018dff1ab
mgi166 <skskoari@gmail.com>
2015-05-23 23:14:31 +0900

    Refactor. faster spec

--- spec/valanga/music_spec.rb
+++ spec/valanga/music_spec.rb
@@ -1,24 +1,27 @@
 RSpec.describe Valanga::Music do
-  let(:music)   { Valanga::Music.new(session) }
-  let(:session) { Valanga::Client.new(ENV['KID'], ENV['K_PASSWORD']).session }
-
   describe '#list_musics' do
+    # NOTE: Spec is too slow, so caches the capybara session.
+    before(:all) do
+      session = Valanga::Client.new(ENV['KID'], ENV['K_PASSWORD']).session
+      @music = Valanga::Music.new(session)
+    end
+
     context 'with valid parameters' do
       it do
         expect do
-          music.list_musics
+          @music.list_musics
         end.not_to raise_error
       end
 
       it do
         expect do
-          music.list_musics(page: 1)
+          @music.list_musics(page: 1)
         end.not_to raise_error
       end
 
       it do
         expect do
-          music.list_musics(sorttype: :music_name)
+          @music.list_musics(sorttype: :music_name)
         end.not_to raise_error
       end
     end
@@ -27,7 +30,7 @@ RSpec.describe Valanga::Music do
       context 'given `page` is not integer' do
         it do
           expect do
-            music.list_musics(page: "aaa")
+            @music.list_musics(page: "aaa")
           end.to raise_error ArgumentError
         end
       end
@@ -35,7 +38,7 @@ RSpec.describe Valanga::Music do
       context 'given `sorttype` is not included in `music_name` or `basic` or ` medium` or `hard` or `special`' do
         it do
           expect do
-            music.list_musics(sorttype: "aaa")
+            @music.list_musics(sorttype: "aaa")
           end.to raise_error ArgumentError
         end
       end
