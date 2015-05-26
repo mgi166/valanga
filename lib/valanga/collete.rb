7f394fba70f74ef8cd37bdc672bf110d436858fb
mgi166 <skskoari@gmail.com>
2015-05-26 10:06:21 +0900

    Add Valanga::Collete::Basic

---
+++ lib/valanga/collete.rb
@@ -0,0 +1,10 @@
+module Valanga
+  class Collete
+    class Basic
+      attr_accessor :rank, :score, :achievement_rate, :miss_count, :play_count
+
+      def initialize(parameter)
+      end
+    end
+  end
+end
