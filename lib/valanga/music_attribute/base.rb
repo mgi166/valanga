6221ded87c923ef03c81dc4bded96821257787d3
mgi166 <skskoari@gmail.com>
2015-05-27 22:08:30 +0900

    def_delegators :@attribute, :has_special?, :music_info_box, :music_bk

--- lib/valanga/music_attribute/base.rb
+++ lib/valanga/music_attribute/base.rb
@@ -1,9 +1,15 @@
+require 'forwardable'
+
 module Valanga
   class MusicAttribute
     class Base
+      extend Forwardable
+
       def initialize(attribute)
         @attribute = attribute
       end
+
+      def_delegators :@attribute, :has_special?, :music_info_box, :music_bk
     end
   end
 end
