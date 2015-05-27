cce4d5db5cff8f119712519b275c6b66c1ade3d3
mgi166 <skskoari@gmail.com>
2015-05-27 22:08:56 +0900

    Remove `groovin_` prefix because that methods is `Groovin` instance method

--- lib/valanga/music_attribute/groovin.rb
+++ lib/valanga/music_attribute/groovin.rb
@@ -12,19 +12,19 @@ module Valanga
         full_combo
       )
 
-      def groovin_basic
+      def basic
         Hash[GROOVIN_KEY.zip(music_info_box[1])]
       end
 
-      def groovin_medium
+      def medium
         Hash[GROOVIN_KEY.zip(music_info_box[2])]
       end
 
-      def groovin_hard
+      def hard
         Hash[GROOVIN_KEY.zip(music_info_box[3])]
       end
 
-      def groovin_special
+      def special
         Hash[GROOVIN_KEY.zip(music_info_box[4])] if has_special?
       end
     end
