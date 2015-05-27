cf3247be90708f02932ff059466f3845d7c10578
mgi166 <skskoari@gmail.com>
2015-05-27 22:48:59 +0900

    Add spec MusicAttribute#collete #groovin #has_special? * forget requirement
    *

--- spec/valanga/music_attribute_spec.rb
+++ spec/valanga/music_attribute_spec.rb
@@ -1,4 +1,24 @@
 RSpec.describe Valanga::MusicAttribute do
+  describe '#collete' do
+    let(:doc) { document('Arousing') }
+
+    it do
+      expect(
+        Valanga::MusicAttribute.new(doc).collete
+      ).to be_instance_of Valanga::MusicAttribute::Collete
+    end
+  end
+
+  describe '#groovin' do
+    let(:doc) { document('Arousing') }
+
+    it do
+      expect(
+        Valanga::MusicAttribute.new(doc).groovin
+      ).to be_instance_of Valanga::MusicAttribute::Groovin
+    end
+  end
+
   describe '#has_special?' do
     subject { Valanga::MusicAttribute.new(doc).has_special? }
 
