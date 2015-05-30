8ce325c409bb88a9f8a759303964affd05c41ccb
mgi166 <skskoari@gmail.com>
2015-05-30 21:23:48 +0900

    Add Difficulty::Base, and refactor

---
+++ lib/valanga/difficulty/base.rb
@@ -0,0 +1,53 @@
+module Valanga
+  module Difficulty
+    class Base
+      def rank
+        groovin.rank || collete.rank
+      end
+
+      def score
+        if groovin.score >= collete.score
+          groovin.score
+        else
+          collete.score
+        end
+      end
+
+      def achievement_rate
+        if groovin.achievement_rate >= collete.achievement_rate
+          groovin.achievement_rate
+        else
+          collete.achievement_rate
+        end
+      end
+
+      def miss_count
+        if [groovin.miss_count, collete.miss_count].all?
+          if groovin.miss_count >= collete.miss_count
+            collete.miss_count
+          else
+            groovin.miss_count
+          end
+        else
+          groovin.miss_count || collete.miss_count
+        end
+      end
+
+      def play_count
+        if groovin.play_count >= collete.play_count
+          groovin.play_count
+        else
+          collete.play_count
+        end
+      end
+
+      def clear
+        groovin.clear || collete.clear
+      end
+
+      def full_combo
+        groovin.full_combo || collete.full_combo
+      end
+    end
+  end
+end
--- lib/valanga/difficulty/medium.rb
+++ lib/valanga/difficulty/medium.rb
@@ -1,6 +1,8 @@
 module Valanga
   module Difficulty
-    class Medium
+    class Medium < Base
+      attr_reader :collete, :groovin
+
       def initialize(music)
         @collete = music.collete.medium
         @groovin = music.groovin.medium
@@ -8,54 +10,6 @@ module Valanga
 
       def difficulty
         'MEDIUM'
-      end
-
-      def rank
-        @groovin.rank || @collete.rank
-      end
-
-      def score
-        if @groovin.score >= @collete.score
-          @groovin.score
-        else
-          @collete.score
-        end
-      end
-
-      def achievement_rate
-        if @groovin.achievement_rate >= @collete.achievement_rate
-          @groovin.achievement_rate
-        else
-          @collete.achievement_rate
-        end
-      end
-
-      def miss_count
-        if [@groovin.miss_count, @collete.miss_count].all?
-          if @groovin.miss_count >= @collete.miss_count
-            @collete.miss_count
-          else
-            @groovin.miss_count
-          end
-        else
-          @groovin.miss_count || @collete.miss_count
-        end
-      end
-
-      def play_count
-        if @groovin.play_count >= @collete.play_count
-          @groovin.play_count
-        else
-          @collete.play_count
-        end
-      end
-
-      def clear
-        @groovin.clear || @collete.clear
-      end
-
-      def full_combo
-        @groovin.full_combo || @collete.full_combo
       end
     end
   end
