e7730becfa9b4eb81f7e454d1bb572d4455adc17
mgi166 <skskoari@gmail.com>
2015-05-30 19:53:37 +0900

    Add `Valanga::Difficulty::Basic` class

--- lib/valanga/basic.rb
+++
@@ -1,8 +0,0 @@
-module Valanga
-  class Basic
-    attr_accessor :rank, :score, :achievement_rate, :miss_count, :play_count, :clear
-
-    def initialize(parameter)
-    end
-  end
-end
---
+++ lib/valanga/difficulty/basic.rb
@@ -0,0 +1,8 @@
+module Valanga
+  module Difficulty
+    class Basic
+      def initialize
+      end
+    end
+  end
+end
