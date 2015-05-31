b126c8845a1e3eecf51db6d94358a6c85de79394
mgi166 <skskoari@gmail.com>
2015-05-31 12:01:16 +0900

    Updates spec Music::Collete

--- spec/valanga/music/collete_spec.rb
+++ spec/valanga/music/collete_spec.rb
@@ -5,33 +5,13 @@ RSpec.describe Valanga::Music::Collete do
     context 'Arousing' do
       let(:attribute) { Valanga::Music.new(document('Arousing')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_4.png",
-            "score"            => "378",
-            "achievement_rate" => "91.9%",
-            "miss_count"       => "0",
-            "play_count"       => "1",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
 
     context 'Anisakis' do
       let(:attribute) { Valanga::Music.new(document('Anisakis')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_5.png",
-            "score"            => "787",
-            "achievement_rate" => "98.6%",
-            "miss_count"       => "0",
-            "play_count"       => "1",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
   end
 
@@ -41,33 +21,13 @@ RSpec.describe Valanga::Music::Collete do
     context 'Arousing' do
       let(:attribute) { Valanga::Music.new(document('Arousing')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_5.png",
-            "score"            => "939",
-            "achievement_rate" => "96.1%",
-            "miss_count"       => "0",
-            "play_count"       => "3",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
 
     context 'Anisakis' do
       let(:attribute) { Valanga::Music.new(document('Anisakis')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_4.png",
-            "score"            => "1331",
-            "achievement_rate" => "93.9%",
-            "miss_count"       => "0",
-            "play_count"       => "2",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
   end
 
@@ -77,33 +37,13 @@ RSpec.describe Valanga::Music::Collete do
     context 'Arousing' do
       let(:attribute) { Valanga::Music.new(document('Arousing')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_5.png",
-            "score"            => "1611",
-            "achievement_rate" => "98.1%",
-            "miss_count"       => "0",
-            "play_count"       => "46",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
 
     context 'Anisakis' do
       let(:attribute) { Valanga::Music.new(document('Anisakis')) }
 
-      it do
-        is_expected.to eq(
-          {
-            "rank"             => "/game/reflec/groovin/p/images/music/rank/syousai_4.png",
-            "score"            => "2077",
-            "achievement_rate" => "93.0%",
-            "miss_count"       => "1",
-            "play_count"       => "50",
-          }
-        )
-      end
+      it { is_expected.to be_instance_of Valanga::MusicAttribute }
     end
   end
 end
