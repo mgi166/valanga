744dac93825abb22b01648f1b323b5c3911e2c6f
mgi166 <skskoari@gmail.com>
2015-06-13 18:09:21 +0900

    Refactor Music::Groovin

--- lib/valanga/music/groovin.rb
+++ lib/valanga/music/groovin.rb
@@ -13,39 +13,30 @@ module Valanga
       )
 
       def basic
-        Valanga::MusicAttribute.new(create_attr(1)).tap do |attr|
-          attr.name   = name
-          attr.artist = artist
-        end
+        Valanga::MusicAttribute.new(create_attr(1))
       end
 
       def medium
-        Valanga::MusicAttribute.new(create_attr(2)).tap do |attr|
-          attr.name   = name
-          attr.artist = artist
-        end
+        Valanga::MusicAttribute.new(create_attr(2))
       end
 
       def hard
-        Valanga::MusicAttribute.new(create_attr(3)).tap do |attr|
-          attr.name   = name
-          attr.artist = artist
-        end
+        Valanga::MusicAttribute.new(create_attr(3))
       end
 
       def special
         if has_special?
-          Valanga::MusicAttribute.new(create_attr(4)).tap do |attr|
-            attr.name   = name
-            attr.artist = artist
-          end
+          Valanga::MusicAttribute.new(create_attr(4))
         end
       end
 
       private
 
       def create_attr(index)
-        Hash[GROOVIN_KEY.zip(music_info_box[index])]
+        Hash[GROOVIN_KEY.zip(music_info_box[index])].merge(
+          'name'   => name,
+          'artist' => artist,
+        )
       end
     end
   end
