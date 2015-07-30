ee5880c42721786eda4047e46e6699792aa183d4
mgi166 <skskoari@gmail.com>
2015-07-30 23:46:02 +0900

    Add #play_history * cache from parsed html document

--- lib/valanga/play_list_search.rb
+++ lib/valanga/play_list_search.rb
@@ -4,5 +4,14 @@ module Valanga
 
     def history(num = 4)
     end
+
+    private
+
+    def play_history
+      @play_history ||= (
+        @session.visit MUSIC_PLAY_LIST
+        Nokogiri::HTML.parse(@session.body).css("#mybest_list tr").shift
+      )
+    end
   end
 end
